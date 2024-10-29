8e13f8b5a755edc0488b32cf777b505e9ad60379
EC2 Default User <ec2-user@ip-172-31-37-118.eu-west-1.compute.internal>
2024-10-29 10:38:35 +0000

    breaking code

--- src/main/java/com/pgr301/exam/ReallyShakyBankingCoreSystemService.java
+++ src/main/java/com/pgr301/exam/ReallyShakyBankingCoreSystemService.java
@@ -58,7 +58,7 @@ class ReallyShakyBankingCoreSystemService implements BankingCoreSystmeService {
 
     private Account getOrCreateAccount(String accountId) {
         if (theBank.get(accountId) == null) {
-            Account a = new Account();
+        
             a.setId(accountId);
             theBank.put(accountId, a);
         }
